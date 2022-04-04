5d7caa39b08a37e298e11a2fb3b86ce2734c451b
NhatRmit <62049702+NhatRmit@users.noreply.github.com>
2022-04-04 10:42:37 +0700

    backend (delete comment, filter, search), fix small bug for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,16 @@
 import React, {useEffect} from 'react'
-import {BrowseRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-//redux
-import {Provider} from 'react-redux'
-import store from './app/store'
-
+//components
 
 function App() {
   return (
-    <Provider store={store}>
       <Router>
-        
         <Routes>
           <Route />
 
         </Routes>
       </Router>
-    </Provider> 
   );
 }
 
---
+++ client/src/reducers/post.js
@@ -0,0 +1,10 @@
+const initialState = {
+    posts: [],
+    post: null,
+    loading: true,
+    error: {}
+  };
+
+  function postReducer(state = initialState, action) {}
+
+  export default postReducer
