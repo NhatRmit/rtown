391ac195d1ad163cd2973d777ca0fc1312cf9e19
minhthuhua72 <s3878667@rmit.edu.vn>
2022-04-07 21:02:06 +0700

    update footer

--- client/src/components/Footer/LoginFooter.jsx
+++ client/src/components/Footer/LoginFooter.jsx
@@ -1,4 +1,4 @@
-import style from "./LoginFooter.css";
+import style from "./LoginFooter.module.css";
 import {Link} from "react-router-dom";
 
 const LoginFooter = () => {
@@ -8,9 +8,11 @@ const LoginFooter = () => {
         <footer className={style["loginform-footer"]}>
             <div className={[style["copyright"]]}>Copyright &copy; {currentYear} RMIT Town. All Rights Reserved.</div>
             <div className={style["footer-nav"]}>
-                    <Link to="/">Disclaimer</Link>
-                    <Link to="/">Terms</Link>
-                    <Link to="/">Privacy</Link>
+                <Link to="/">Disclaimer</Link>
+                <div className={style["vl"]}></div>
+                <Link to="/">Terms</Link>
+                <div className={style["vl"]}></div>
+                <Link to="/">Privacy</Link>
             </div>
       </footer>
   )
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,6 @@ import LoginNavbar from '../components/Navbar/LoginNavbar'
 import LoginForm from '../components/Auth/LoginForm'
 import LoginFooter from '../components/Footer/LoginFooter'
 
-
 const Login = () => {
     return(
         <>
