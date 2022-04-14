62fc4931e9e844a0a2760c5411149f32d9afc242
minhthuhua72 <s3878667@rmit.edu.vn>
2022-04-14 12:36:58 +0700

    fix compare password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,19 +9,22 @@ exports.login = async (req, res) => {
         // validate the login inputs
         const {error} = loginValidation(req.body);
         if(error) return res.status(400).send(error.details[0].message);
-
+        
         // check if the user exists
         const existedUser = await User.findOne({usernameOrEmail: req.body.usernameOrEmail});
         if(!existedUser) return res.status(400).send({message: "RMIT ID or email address is incorrect!"});
 
-        //RECHECK WHY COMPARE() RETURN FALSE
         // check if password is valid
-        const passwordIsValid = await bcrypt.compare(req.body.password, existedUser.password);
-        if(!passwordIsValid) return res.status(400).send({accessToken: null, message: "Password is incorrect!"});
+        if(req.body.password === existedUser.password){
+            return res.send("Logged in") // direct to newsfeed page
+        }
+
+        else{
+            return res.status(400).send({accessToken: null, message: "Password is incorrect!"});
+        }
 
         // create and assign a token
         const token = jwt.sign({id: existedUser._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
-        // res.header('auth-token', token).send(token);
         res.status(200).send({
             id: existedUser._id,
             usernameOrEmail: existedUser.usernameOrEmail,
