6e76e1cbdc579471b8bf5855209082aef96a849e
minhthuhua72 <s3878667@rmit.edu.vn>
2022-04-19 01:05:14 +0700

    update newsfeed ui

--- client/src/components/Newsfeed/CommunitySection.jsx
+++ client/src/components/Newsfeed/CommunitySection.jsx
@@ -5,28 +5,28 @@ import { IconContext } from 'react-icons/lib'
 import logo from './media/mobi-logo.jpg'
 
 const CommunitySection = () => {
-    const community = [
-        {
-            id: 1,
-            img: "badminton-club-logo",
-            title: "Badminton Club"
-        },
-        {
-            id: 2,
-            img: "fintech-club-logo",
-            title: "Fintech Club"
-        },
-        {
-            id: 3,
-            img: "mass-media-club-logo",
-            title: "Mass Media Club"
-        },
-        {
-            id: 4,
-            img: 'mobi-logo',
-            title: "Global Experience"
-        }
-    ]
+    // const community = [
+    //     {
+    //         id: 1,
+    //         img: "badminton-club-logo",
+    //         title: "Badminton Club"
+    //     },
+    //     {
+    //         id: 2,
+    //         img: "fintech-club-logo",
+    //         title: "Fintech Club"
+    //     },
+    //     {
+    //         id: 3,
+    //         img: "mass-media-club-logo",
+    //         title: "Mass Media Club"
+    //     },
+    //     {
+    //         id: 4,
+    //         img: 'mobi-logo',
+    //         title: "Global Experience"
+    //     }
+    // ]
 
     return (
        <div className="community-container">
@@ -41,14 +41,15 @@ const CommunitySection = () => {
                 <p>Create community</p>
            </div>
            <div className='joined-community'>
-            {community.map(element => (
+            {/* {community.map(element => (
                 <span className='community' key={element.id}>
                     <Link to='/'> <img src={require('./media/' + community.img + '.jpg')} alt="Community logo" className={community.title}/></Link>
                 </span> 
-            ))}
-            
-
-            {/* <span className='community'>
+            ))} */}
+            <span className='community'>
+                <Link to='/'> <img src={logo} alt="Community logo" className="community-logo"/></Link>
+            </span>
+            <span className='community'>
                 <Link to='/'> <img src={logo} alt="Community logo" className="community-logo"/></Link>
             </span>
             <span className='community'>
@@ -56,7 +57,7 @@ const CommunitySection = () => {
             </span>
             <span className='community'>
                 <Link to='/'> <img src={logo} alt="Community logo" className="community-logo"/></Link>
-            </span> */}
+            </span>
 
            </div>
            
--- client/src/components/Newsfeed/PostsSection.jsx
+++ client/src/components/Newsfeed/PostsSection.jsx
@@ -2,6 +2,7 @@ import './PostsSection.css'
 import { BsFillChatDotsFill } from 'react-icons/bs'
 import { IconContext } from 'react-icons/lib'
 import {Link} from "react-router-dom"
+import PostImage from "./media/rmit-image.png"
 
 const PostsSection = () => {
     return (
@@ -43,6 +44,66 @@ const PostsSection = () => {
                     </div>
                     <div className='post-content'>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin dictum nulla eu hendrerit. Donec commodo fringilla sollicitudin. Duis sit amet ligula quis tellus scelerisque pulvinar ut vitae ligula. Phasellus lectus felis, convallis sit amet consequat quis, interdum eu lectus. Suspendisse potenti. Maecenas at pulvinar lorem. Proin at tellus viverra, lacinia massa nec, rhoncus ligula.</p>
+                    </div>
+                    <div className='content-section-footer'>
+                        <span className='icon'>
+                            <Link to="/">
+                                <label htmlFor='comment'>
+                                    <IconContext.Provider value={{ color: '#676767', size: '1.5em' }}>
+                                        <BsFillChatDotsFill />
+                                    </IconContext.Provider>
+                                </label>
+                            </Link>
+                            <p id="comment"  className='icon-label'> Comments </p>
+                        </span>
+
+                        <span className='icon'>
+                            <Link to="/">
+                                <label htmlFor='report'>
+                                    <IconContext.Provider value={{ color: '#676767', size: '1.5em' }}>
+                                        <BsFillChatDotsFill />
+                                    </IconContext.Provider>
+                                </label>
+                            </Link>
+                            <p id="report" className='icon-label'> Report </p>
+                        </span>
+                    </div>
+                </div>
+           </div>
+
+           <div className='post-section'>
+                <div className='vote-section'>
+                    <span className='icon'>
+                        {/*CHANGE ICONS FOR ME */}
+                        <IconContext.Provider value={{ color: '#676767', size: '1.5em' }}>
+                                <BsFillChatDotsFill />
+                        </IconContext.Provider>
+                    </span>
+                    <p className='vote-total'>1000</p>
+                    <span className='icon'>
+                        {/*CHANGE ICONS FOR ME */}
+                        <IconContext.Provider value={{ color: '#676767', size: '1.5em' }}>
+                                <BsFillChatDotsFill />
+                        </IconContext.Provider>
+                    </span>
+                </div>
+
+                <div className='content-section'>
+                    <div className='content-section-header'>
+                        <span className='icon'>
+                            {/*CHANGE ICONS FOR ME */}
+                            <label htmlFor='username'>
+                                <IconContext.Provider value={{ color: '#676767', size: '1.5em' }}>
+                                    <BsFillChatDotsFill />
+                                </IconContext.Provider>
+                            </label>
+                            <p id="username" className='username'> Username </p>
+                        </span>
+                        <p className='uploaded-time'>Posted 1 hour ago</p>
+                    </div>
+                    <div className='post-content'>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sollicitudin dictum nulla eu hendrerit.</p>
+                        <img src={PostImage} alt="Post Image" className='post-image'></img>
                     </div>
                     <div className='content-section-footer'>
                         <span className='icon'>
