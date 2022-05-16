5286ef4ae1e4bb868a09bd9616ad393049059912
s3818202 <s3818202@rmit.edu.vn>
2022-05-16 22:42:24 +0700

    Version 1.0.0

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./configs/database');
 const cors = require('cors')
 const app = express();
+const path = require('path');
 require('dotenv').config();
 connectDB();
 
@@ -16,6 +17,17 @@ app.use('/api/items', require('./routes/api/itemRoute'))
 app.use('/api/messengers', require('./routes/api/messengerRoute'))
 app.use('/api/images', require('./routes/api/imageRoute'))
 app.use('/api/admins', require('./routes/api/adminRoute'))
+
+
+// serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server started on port ${port}`));
